54f6b55dee497b7b29819546f47c090f39e0d7d6
Zaur Ismailov <zaur.ismailov.dev@gmail.com>
2023-08-29 08:58:01 -0400

    Add app2

---
+++ app2/src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./index.scss";
+
+export default function App() {
+  return (
+    <div className="mt-10 text-3xl mx-auto max-w-6xl">
+      <div>Name: app2</div>
+      <div>Framework: react</div>
+      <div>Language: TypeScript</div>
+      <div>CSS: Tailwind</div>
+    </div>
+  );
+}
